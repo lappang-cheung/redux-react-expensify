31587fe3dcb6ee766f44c9a3d32f4075ed23ba3c
Leo Cheung <leocheung_1986@hotmail.com>
2018-04-08 17:11:45 -0400

    heroku build

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var path = require('path');
-var app = express();
+const http = require('http');
 
 var publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+var app = express();
 
 app.use(express.static(publicPath));
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server online')
 });
