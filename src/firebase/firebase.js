c31a3b39bb42394e6ea14b743b5afa37c5bf5ffd
Leo Cheung <lappangcheung@gmail.com>
2018-04-09 16:54:52 -0400

    testing firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBIkmGzfFebT0fLxvllQZ31XeL0meAzI8A",
+    authDomain: "expensify-edd21.firebaseapp.com",
+    databaseURL: "https://expensify-edd21.firebaseio.com",
+    projectId: "expensify-edd21",
+    storageBucket: "expensify-edd21.appspot.com",
+    messagingSenderId: "997390728720"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+    name: "Leo"
+});
