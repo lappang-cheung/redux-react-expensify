d80c2aad09a35b1c59cdbdf1bc2df8fb260b7d57
Leo Cheung <lappangcheung@gmail.com>
2018-04-10 17:42:20 -0400

    add firebase and create expense done

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,36 @@ const config = {
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-    name: "Leo"
-});
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         })
+//     });
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'Fun four',
+//     note: 'Note four',
+//     amount: 0,
+//     createdAt: 0
+// });
