987cab915e7263e14f6aa9bdbfa1fae4282218a6
Leo Cheung <lappangcheung@gmail.com>
2018-04-11 13:57:18 -0400

    read and write the correct users data

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,14 +9,14 @@ import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
-
+import PublicRoute from './PublicRoute';
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
